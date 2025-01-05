bc53e26c085a2e5d9a7430a12d62f6f4f85797f0
Kdaito <kdaito0620@gmail.com>
2025-01-05 12:38:51 +0900

    エラー時のログ内容を修正

--- internal/application/middleware/logger.go
+++ internal/application/middleware/logger.go
@@ -18,15 +18,7 @@ func LoggerMiddleware() echo.MiddlewareFunc {
 		LogLatency:   true,
 		HandleError:  true,
 		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
-			if v.Error != nil {
-				// example log format
-				// [2021-09-01 00:00:00] method=GET uri=/api/v1/users status=500 error=internal server error duration=1.2345ms UserAgent=Mozilla/5.0
-				fmt.Printf("[%v] method=%v uri=%v, status=%v error='%v' duration=%v UserAgent=%v\n", time.Now().Format("2006-01-02 15:04:05"), v.Method, v.URI, v.Status, v.Error, v.Latency, v.UserAgent)
-			} else {
-				// example log format
-				// [2021-09-01 00:00:00] method=GET uri=/api/v1/users status=200 duration=1.2345ms UserAgent=Mozilla/5.0
-				fmt.Printf("[%v] method=%v uri=%v, status=%v duration=%v UserAgent=%v\n", time.Now().Format("2006-01-02 15:04:05"), v.Method, v.URI, v.Status, v.Latency, v.UserAgent)
-			}
+			fmt.Printf("[%v] method=%v uri=%v, status=%v duration=%v UserAgent=%v\n", time.Now().Format("2006-01-02 15:04:05"), v.Method, v.URI, v.Status, v.Latency, v.UserAgent)
 			return nil
 		},
 	})
